f6e07b31b749cafc4773d62dd316bb60503483c0
Simon Balean <simonbalean@gmail.com>
2018-11-27 08:21:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,15 +1,4 @@
-def my_collect(array)
-  i = 0
-  new_array = []
-  
-  while i < array.size
-    new_array << yield(array[i])
-    i = i + 1 
-  end
-  
-  new_array
-  
+def begins_with_r(array)
+  array.
 end
-
-def begins_with_r
   
