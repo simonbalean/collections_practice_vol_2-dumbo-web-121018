a3af649c7258f2749110877b246ad105053dcc6d
Simon Balean <simonbalean@gmail.com>
2018-12-03 01:07:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
-# your code goes here
+require 'pry'
+
+def begins_with_r(array)
+  array.all? do |x|
+    x.start_with?("r")
+  end
+end
+
+def contain_a(array)
+  array.select do |x|
+    x.include?("a")
+  end
+end
+
+def first_wa(array)
+  array.select do |x|
+    x.start_with?("wa")
+  end
+end
+
+def remove_non_strings(array)
+  array.chomp!
+end
+
+def count_elements(array)
+  
+end
+
+def merge_data(array)
+  
+end
+
+def find_cool(array)
+  
+end
+
+def organize_schools(array)
+  
+end
+
+
+
+
